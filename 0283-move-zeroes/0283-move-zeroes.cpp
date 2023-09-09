88c95181f4449a3580a9a003e4bf2350e283847e
Vansh More <94687983+Vanshmore@users.noreply.github.com>
2023-09-10 01:09:33 +0530

    Time: 14 ms (87.53%), Space: 20.2 MB (5.24%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       int nonZeroIndex = 0;
-
-    // Iterate through the array.
-    for (int i = 0; i < nums.size(); ++i) {
-        // If the current element is non-zero, move it to the position pointed by nonZeroIndex.
-        if (nums[i] != 0) {
-            std::swap(nums[i], nums[nonZeroIndex]);
-            nonZeroIndex++;
+        int n = nums.size();
+        vector<int> temp;
+        for(int i = 0; i<n; i++){
+            if(nums[i]!=0){
+                temp.push_back(nums[i]);
+            }
         }
-    }
+        int nz = temp.size();
+        for(int i = 0; i < nz; i++){
+            nums[i]=temp[i];
+        }
+        for(int i = nz; i<n; i++){
+            nums[i]=0;
+        }
     }
 };
