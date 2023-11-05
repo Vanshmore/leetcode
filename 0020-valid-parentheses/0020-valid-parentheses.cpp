a7a28cbd1b5066603cee3f6761fecc24f60b888c
Vansh More <94687983+Vanshmore@users.noreply.github.com>
2023-11-05 22:31:06 +0530

    Time: 3 ms (45.30%), Space: 6.9 MB (9.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-            std::stack<char> parenthesesStack;
-    std::unordered_map<char, char> mapping = {{')', '('}, {']', '['}, {'}', '{'}};
-    
-    for (char c : s) {
-        if (mapping.find(c) != mapping.end()) {
-            char topElement = parenthesesStack.empty() ? '#' : parenthesesStack.top();
-            parenthesesStack.pop();
-            if (topElement != mapping[c]) {
-                return false;
+        stack<int>st;
+        for(int i=0; i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
             }
-        } else {
-            parenthesesStack.push(c);
+            else{
+                if(st.empty()==true){
+                    return false;
+                }
+                else if(matching(st.top(),s[i])==false){
+                    return false;
+                }
+                else{
+                    st.pop();
+                }
+            }
         }
-    }
-    
-    return parenthesesStack.empty();
-    }
+        return (st.empty()==true);      
+};
+bool matching(char a, char b){
+    return((a=='('&&b==')')||
+           (a=='{'&&b=='}')||
+           (a=='['&&b==']'));
+            
+}
 };
