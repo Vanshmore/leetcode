02b7124f77510fd0586acdff4fc5aab78361e689
Vansh More <94687983+Vanshmore@users.noreply.github.com>
2024-03-10 11:24:45 +0530

    Time: 18 ms (9.75%), Space: 10.2 MB (10.43%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,22 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0)
-            return false;
-
-        int original = x;
-        int reversed = 0;
-
-        while (x != 0) {
-            int digit = x % 10;
-            if (reversed > (INT_MAX - digit) / 10) {
-                // Handling potential overflow
-                return false;
-            }
-            reversed = reversed * 10 + digit;
-            x /= 10;
-        }
-
-        return original == reversed;
+        string str_x = to_string(x);
+        return str_x == string(str_x.rbegin(), str_x.rend());
     }
 };
